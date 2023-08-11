887e42fbbaac470dab889e8d8180bad38405644f
nadirbasalamah <nadir008basalamah@gmail.com>
2023-08-11 08:17:46 +0700

    feat(controller): add controller

---
+++ src/main/java/com/example/springjwtapi/auth/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.example.springjwtapi.auth;
+
+import com.example.springjwtapi.model.AuthenticationRequest;
+import com.example.springjwtapi.model.AuthenticationResponse;
+import com.example.springjwtapi.model.RegisterRequest;
+import com.example.springjwtapi.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(
+            @RequestBody AuthenticationRequest request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
